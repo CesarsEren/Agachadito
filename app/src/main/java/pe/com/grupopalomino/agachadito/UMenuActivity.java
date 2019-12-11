94491f15f7c24f56ac7ca3b5b1f23dc261891335
CesarsEren <cpinedo428@gmail.com>
2019-12-10 23:45:45 -0500

    Commit Maps

--- app/src/main/java/pe/com/grupopalomino/agachadito/UMenuActivity.java
+++ app/src/main/java/pe/com/grupopalomino/agachadito/UMenuActivity.java
@@ -1,9 +1,15 @@
 package pe.com.grupopalomino.agachadito;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +19,8 @@ import pe.com.grupopalomino.agachadito.Utils.spacetablayout.SpaceTabLayout;
 public class UMenuActivity extends AppCompatActivity {
 
     SpaceTabLayout tabLayout;
+    private final int PERMISO_GPS=1;
+    private boolean tienePermiso = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +38,49 @@ public class UMenuActivity extends AppCompatActivity {
 
         tabLayout.initialize(viewPager, getSupportFragmentManager(),
                 fragmentList);
+
+        int currentApiVersion = Build.VERSION.SDK_INT;
+        if(currentApiVersion >= Build.VERSION_CODES.M)
+        {
+            validarUsoUbicacion();
+        }else
+        {
+            tienePermiso = true;
+        }
+
     }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void validarUsoUbicacion() {
+        final int IGPS = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(IGPS!= PackageManager.PERMISSION_GRANTED)
+        {
+            String[]permisos = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+            requestPermissions(permisos,PERMISO_GPS);;
+        }else
+        {
+            tienePermiso = true;
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==PERMISO_GPS)
+        {
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                tienePermiso = true;
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),"No podrá hacer uso del GPS",Toast.LENGTH_SHORT).show();
+                tienePermiso=false;
+            }
+        }
+    }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         //tabLayout.saveState(outState);
--- app/src/main/java/pe/com/grupopalomino/agachadito/URegistrarClienteActivity.java
+++ app/src/main/java/pe/com/grupopalomino/agachadito/URegistrarClienteActivity.java
@@ -1,13 +1,73 @@
 package pe.com.grupopalomino.agachadito;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class URegistrarClienteActivity extends AppCompatActivity {
+    private final int PERMISO_GPS = 1;
+    private boolean tienePermiso = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uregistrar_cliente);
+
+        int currentApiVersion = Build.VERSION.SDK_INT;
+        if (currentApiVersion >= Build.VERSION_CODES.M) {
+            validarUsoUbicacion();
+        } else {
+            tienePermiso = true;
+        }
+
+
+        Button btnregistrar = findViewById(R.id.btnregistrar);
+        btnregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tienePermiso) {
+                    Intent intentMap = new Intent().setClass(getBaseContext(), URegistroP2Activity.class);
+                    startActivity(intentMap);
+                } else {
+                    Toast.makeText(getApplicationContext(), "No tiene permiso para acceder a su GPS", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void validarUsoUbicacion() {
+        final int IGPS = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (IGPS != PackageManager.PERMISSION_GRANTED) {
+            String[] permisos = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+            requestPermissions(permisos, PERMISO_GPS);
+            ;
+        } else {
+            tienePermiso = true;
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISO_GPS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                tienePermiso = true;
+            } else {
+                Toast.makeText(getApplicationContext(), "No podrá hacer uso del GPS", Toast.LENGTH_SHORT).show();
+                tienePermiso = false;
+            }
+        }
     }
 }
